4e7c23d78dc0da315fb1404f8248ffef1a047691
BKHOMEDESKTOP\boonk <john.kuae@gmail.com>
2024-11-09 20:43:07 +0700

    Init

---
+++ section6/v1-springboot/Accounts/src/main/java/com/bk/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,46 @@
+package com.bk.eazybytes.accounts;
+
+import com.bk.eazybytes.accounts.DTO.AccountContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.accounts.controller") })
+@EnableJpaRepositories("com.eazybytes.accounts.repository")
+@EntityScan("com.eazybytes.accounts.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = AccountContactInfoDto.class)
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "BK",
+						email = "bk@mail.com",
+						url = "https://www.bk.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.bk.com"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation",
+				url = "https://www.bk.com/swagger-ui.html"
+		)
+)
+public class AccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsApplication.class, args);
+	}
+
+}
